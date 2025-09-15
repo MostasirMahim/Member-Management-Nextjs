3a52d74b5c6b2dc06668664bb2b4b91c6190c703
Mostasir Mahim <mahimlive@gmail.com>
2025-09-15 16:25:15 +0600

    feat(theme toggle): implement themes chenge/toggle features.

--- src/providers/ThemeProviders.tsx
+++ src/providers/ThemeProviders.tsx
@@ -2,10 +2,18 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useThemeStore } from "@/store/theme_store";
+import { cn } from "@/lib/utils";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const { theme } = useThemeStore();
+  return (
+    <NextThemesProvider {...props}>
+      {" "}
+      <div className={cn("antialiased", `theme-${theme}`)}>{children}</div>
+    </NextThemesProvider>
+  );
 }
---
+++ src/store/theme_store.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+import { persist, createJSONStorage } from "zustand/middleware";
+
+interface ThemeStoreState {
+  theme: string;
+  setTheme: (theme: string) => void;
+}
+
+export const useThemeStore = create<ThemeStoreState>()(
+  persist(
+    (set) => ({
+      theme: "zinc",
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: "theme-store",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+)
