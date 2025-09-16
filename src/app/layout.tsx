17f2ac80137ec5b67613576cc3a88e89b51a9853
Mostasir Mahim <mahimlive@gmail.com>
2025-09-16 20:51:38 +0600

    perf(layout): update root layout with providers.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable}"`}
+    >
+      <body className={`antialiased`}>
         <Providers>
           <ThemeProvider
             attribute="class"
