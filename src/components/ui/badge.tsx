954d3a2b98698bed13a456fa9869f3179ac1ea3b
Mostasir Mahim <mahimlive@gmail.com>
2025-09-18 15:58:14 +0600

    perf(shadcn): match with theme classes in ui components.

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,7 +9,7 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:bg-secondary",
+          "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 dark:hover:bg-accent dark:hover:text-accent-foreground",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-primary hover:text-white dark:hover:bg-accent dark:hover:text-accent-foreground",
         destructive:
