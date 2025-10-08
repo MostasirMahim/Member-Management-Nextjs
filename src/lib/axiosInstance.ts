029905aac5d77b8b350f547ae6b2e9c4fb6c3ed1
Mostasir Mahim <mahimlive@gmail.com>
2025-10-09 03:15:27 +0600

    refactor : added intercepters response part.

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -10,4 +10,36 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+    const currentPath = typeof window !== "undefined" ? window.location.pathname : "";
+
+    if (status && typeof window !== "undefined") {
+      switch (status) {
+        case 401:
+          if (currentPath !== "/login") {
+            window.location.replace("/login?expired=true");
+          }
+          break;
+
+        case 403:
+          window.location.replace("/unauthorized");
+          break;
+
+        case 405:
+          window.location.replace("/");
+          break;
+        
+        case 500:
+          window.location.replace(`/error?code=500`);
+          break;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
